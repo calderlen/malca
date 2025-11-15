70439926c8960b35d932e5f83d97b484683a46c0
calderlen <calderlen@gmail.com>
2025-11-15 18:30:07 -0500

    forgot to add the new files in the last commit

---
+++ calder/script_reproduce_candidates.py
@@ -0,0 +1,11 @@
+from reproduce_candidates import build_reproduction_report
+
+report = build_reproduction_report(
+    out_dir="./results_test",   
+    out_format="csv",                 
+    n_workers=1,                    
+)
+
+print(report[["source","source_id","mag_bin",
+              "detected","detection_details",
+              "g_n_peaks","v_n_peaks","matches_expected"]])
